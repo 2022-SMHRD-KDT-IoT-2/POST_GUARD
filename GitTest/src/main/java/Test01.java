d6898f21a2d752096a52f18cb1712ef12257d19d
smhrd <smhrd@222.102.104.76>
2022-02-22 15:50:33 +0900

    한글이 깨져요 ㅜㅡㅜ

--- GitTest/src/main/java/Test01.java
+++ GitTest/src/main/java/Test01.java
@@ -4,7 +4,7 @@ public class Test01 {
 	public static void main(String[] args) {
 
 		System.out.println("welcome to my first project");
-		System.out.println("2¹øÂ° Ä¿¹Ô");
+		System.out.println("2nd commit");
 	}
 
 }
